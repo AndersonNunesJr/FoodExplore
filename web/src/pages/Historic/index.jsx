202c6642f83216786f87ad36f98e38649f409e47
Anderson Nunes de Paula Junior <andersonnunes2015@gmail.com>
2024-04-22 13:54:06 -0300

    .

--- web/src/pages/Historic/index.jsx
+++ web/src/pages/Historic/index.jsx
@@ -1,14 +1,45 @@
 import { Footer, Navbar, Section } from "../../components";
-import { Container } from "./styles";
+import { Container, Board } from "./styles";
 
 export function Historic() {
   return (
     <Container>
       <Navbar />
-      <Section>
-        <h1>Histórico de pedidos</h1>
-      </Section>
+      <h1>Histórico de pedidos</h1>
+      <Board>
+        <h2 className="name left">Status</h2>
+        <h2 className="name">Código</h2>
+        <h2 className="name">Detalhamento</h2>
+        <h2 className="name right">Data e hora</h2>
 
+        <div className="status">
+          <p>dsad</p>
+          <p>dasdas</p>
+          <p>dasdsad</p>
+          <p>adsdasd</p>
+        </div>
+        <div className="code">
+          {" "}
+          <p>dsad</p>
+          <p>dasdas</p>
+          <p>dasdsad</p>
+          <p>adsdasd</p>
+        </div>
+        <div className="details">
+          {" "}
+          <p>dsad</p>
+          <p>dasdas</p>
+          <p>dasdsad</p>
+          <p>adsdasd</p>
+        </div>
+        <div className="data">
+          {" "}
+          <p>dsad</p>
+          <p>dasdas</p>
+          <p>dasdsad</p>
+          <p>adsdasd</p>
+        </div>
+      </Board>
       <Footer />
     </Container>
   );
--- web/src/pages/Historic/styles.js
+++ web/src/pages/Historic/styles.js
@@ -7,4 +7,61 @@ export const Container = styled.div`
   justify-content: space-between;
 
   color: aliceblue;
+
+  > h1 {
+    margin-top: 32px;
+    margin-bottom: 32px;
+    padding-left: 75px;
+  }
+`;
+
+export const Board = styled.div`
+  height: 100%;
+
+  margin: 0 75px 75px;
+
+  display: grid;
+  grid-area: "name" "status" "code" "details" "data";
+  grid-template-columns: 1fr 1fr 5fr 1fr;
+  grid-template-rows: 50px auto;
+
+  p {
+    border-bottom: solid 1px red;
+    padding: 5px 10px;
+  }
+
+  .name {
+    display: flex;
+    align-items: center;
+    border: solid 1px red;
+
+    padding: 5px 10px;
+  }
+  .left {
+    border-radius: 8px 0 0 0;
+  }
+  .right {
+    border-radius: 0 8px 0 0;
+  }
+  .status {
+    border-right: solid 1px red;
+    border-left: solid 1px red;
+    border-bottom: solid 1px red;
+    padding: 5px 0 10px 0;
+  }
+  .code {
+    border-bottom: solid 1px red;
+    padding: 5px 0 10px 0;
+  }
+  .details {
+    border-bottom: solid 1px red;
+    border-right: solid 1px red;
+    border-left: solid 1px red;
+    padding: 5px 0 10px 0;
+  }
+  .data {
+    border-right: solid 1px red;
+    border-bottom: solid 1px red;
+    padding: 5px 0 10px 0;
+  }
 `;
