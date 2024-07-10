53a1ddb5d859457c106e930e217acafcd524ea33
Anderson Nunes de Paula Junior <andersonnunes2015@gmail.com>
2024-07-10 18:05:53 -0300

    .

--- api2/src/controllers/productsController/productsGet.ts
+++ api2/src/controllers/productsController/productsGet.ts
@@ -4,36 +4,74 @@ import z from "zod";
 import { prisma } from "../../lib/prisma";
 
 export async function productsGet(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
+  app.withTypeProvider<ZodTypeProvider>().post(
     "/",
     {
       schema: {
         summary: "Get product from all marketplace.",
         tags: ["Get"],
+        body: z.object({
+          name: z.string().nullable()
+        }),
         response: {
           201: z.object({
-            result: z.array(
-              z.object({
-                id: z.string(),
-                title: z.string(),
-                tag: z.string().nullable(),
-                description: z.string().nullable(),
-                category: z.string().nullable(),
-                price: z.string(),
-                productImg: z.string().nullable(),
-                marketplace: z
-                  .object({
-                    storename: z.string().nullable()
-                  })
-                  .nullable()
-              })
-            )
+            result: z
+              .array(
+                z.object({
+                  id: z.string(),
+                  title: z.string(),
+                  tag: z.string().nullable(),
+                  description: z.string().nullable(),
+                  category: z.string().nullable(),
+                  price: z.string(),
+                  productImg: z.string().nullable(),
+                  marketplace: z
+                    .object({
+                      storename: z.string().nullable()
+                    })
+                    .nullable()
+                })
+              )
+              .nullish(),
+            message: z.string().nullish()
           })
         }
       }
     },
     async (req, reply) => {
+      const { name } = req.body;
+
+      if (!name) {
+        const result = await prisma.product.findMany({
+          select: {
+            id: true,
+            title: true,
+            tag: true,
+            description: true,
+            category: true,
+            price: true,
+            productImg: true,
+            marketplace: {
+              select: {
+                storename: true
+              }
+            }
+          }
+        });
+        return reply.status(201).send({ result });
+      }
+
       const result = await prisma.product.findMany({
+        where: {
+          OR: [
+            {
+              title: name
+            },
+            {
+              category: name
+            }
+          ]
+        },
         select: {
           id: true,
           title: true,
@@ -49,6 +87,10 @@ export async function productsGet(app: FastifyInstance) {
           }
         }
       });
+
+      if (Array.isArray(result) && result.length === 0) {
+        return reply.status(404).send({ message: "Product not found" });
+      }
 
       return reply.status(201).send({ result });
     }
